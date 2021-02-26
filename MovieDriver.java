d8e2a0f8e9a26d81bb37cd2443f2afac47333774
khaijadija <77999742+khaijadija@users.noreply.github.com>
2021-02-25 20:06:18 -0600

    Create MovieDriver.java

---
+++ MovieDriver.java
@@ -0,0 +1,29 @@
+
+import java.sql.*;
+
+public class MovieDriver {
+
+	
+	public static void main(String[] args) {
+		
+		try {
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb", "root", "");
+		
+			Statement myStmt = myConn.createStatement();
+			
+			//ResultSet myRs = myStmt.executeQuery("Select * from people");
+			ResultSet myRs = myStmt.executeQuery("Select * from movies");
+			
+			while (myRs.next()) {
+				//System.out.println(myRs.getString("first_name") + " " + myRs.getString("last_name"));
+				System.out.println(myRs.getString("english_name"));
+			}
+		
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+
+}
